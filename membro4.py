8658d3b45c7e51da8dc53f99a7a792de4866c79e
unknown <vitorprca@gmail.com>
2023-03-07 16:21:51 -0300

    Adicionado membro4

---
+++ membro4.py
@@ -0,0 +1,2 @@
+def hello():
+    return "Olá, eu me chamo Vitor Praxedes Calegari(informação importante devido ao número de outros vitors nessa faculdade), tenho 18 anos, gosto de jogar e assistir filmes. Ainda sou iniciante na área de desenvolvimento, mas estou ancioso para começarmos nossas atividades para aprender novas coisas e desenvolver minhas habilidades."
