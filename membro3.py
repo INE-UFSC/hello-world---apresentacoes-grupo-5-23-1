7228041667c7c39ac52af0c1e81e357063ccb616
roberto.becker <roberto.becker@nexxera.com>
2023-03-10 09:51:00 -0300

    atualizando o membro 3

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu me chamo Roberto, tenho 20 anos e tenho certa experiencia com desenvolvimento de software pois trabalho com isso há uns 2 anos. Gosto de ver serie, filmes e viajar. Espero aprender muito bem a base e os porques da POO."
+	return "Olá, eu me chamo Roberto, tenho 20 anos e tenho certa experiencia com desenvolvimento de software. Gosto de ver serie, filmes e viajar. Espero aprender muito bem a base e os porques da POO."
